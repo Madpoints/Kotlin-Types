1b64eab15df16894749b4cd69701db5263704cae
John Furtado <john.furtado@virginpulse.com>
2019-05-28 11:19:35 -0400

    class constructors

--- src/rsk/Person.kt
+++ src/rsk/Person.kt
@@ -1,14 +1,24 @@
 package rsk
 
-abstract class Person {
-    var firstName:String = ""
-    var lastName:String = ""
+fun main(args: Array<String>) {
+    val kevin = Student("Kevin", "Day",1, "some tutor")
+    println(kevin.id)
+}
+
+abstract class Person(var firstName:String,  var lastName:String) {
 
     open fun getName():String = "$firstName $lastName"
     abstract fun getAddress():String
 }
 
-class Student:Person() {
+class Student(firstName: String, lastName: String, _id:Int, var tutor: String) : Person(firstName, lastName) {
+    val id : Int
+
+    init {
+        id = _id
+
+    }
+
     override fun getAddress(): String {
         return ""
     }
